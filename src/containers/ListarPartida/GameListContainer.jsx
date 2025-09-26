b9245676513fc48fa106d5a17e7b12fe9a89d806
Kurt_godel110 <andres.vecchiet@mi.un.edu.ar>
2025-09-25 22:32:52 -0300

    SCRUM-21 : Coneccion de listarPartidas con el backend

---
+++ src/containers/ListarPartida/GameListContainer.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { apiService } from "@/services/apiService.js";
+import GameList from "@/components/GameList/GameList.jsx";
+
+
+
+const GameListContainer = () => {
+
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const data = await apiService.listGames();
+        if (!data.error) {
+            setGames(
+                // mapeo a lo que espera GameCard
+                data.map((p) => ({
+                    id: p.id,
+                    name: p.nombre,
+                    minPlayers: p.minJugadores,
+                    maxPlayers: p.maxJugadores,
+                    currentPlayers: p.cantJugadores
+                }))
+            );
+        } else {
+          setError(data.message);
+        }
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchGames();
+  }, []);
+
+  if (error) return <p>Error: {error}</p>;
+
+  return <GameList games={games} />;
+};
+
+export default GameListContainer;
